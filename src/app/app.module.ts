ce372d25f0a02ee0bab7c0d85af4b7abcaf6a30f
Ladner, Denis <Denis.Ladner@EXXETA.com>
2020-02-13 13:36:13 +0100

    add questionnaire and option component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { QuestionnaireComponent } from './questionnaire/questionnaire.component';
+import { OptionComponent } from './option/option.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuestionnaireComponent,
+    OptionComponent
   ],
   imports: [
     BrowserModule
